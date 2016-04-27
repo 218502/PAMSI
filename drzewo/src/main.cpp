88fddf6e5baff323b2832ebd5bd802def0554cdc
218502 <218502@student.pwr.edu.pl>
2016-04-28 00:07:50 +0200

    praca w domu v0.3

--- drzewo/src/main.cpp
+++ drzewo/src/main.cpp
@@ -2,22 +2,24 @@
 #include "element.hh"
 #include "stoper.hh"
 #include "tree.hh"
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
+  const int sup=10000000;// zlozonosc obliczeniowa
   tree *drzewko;
+  Stoper s;
   drzewko=new tree;
-  element e1,e2,e3;
-  e1.key=1;
-  e1.value=15;
-  e2.key=2;
-  e2.value=20;
-  e3.key=3;
-  e3.value=25;
-  drzewko->add(e1);
-  drzewko->add(e2);
-  drzewko->add(e3);
-  cout<<"element o kluczu 2 : "<<drzewko->find(2)->getel().value<<endl;
+  element e1;
+
+  s.start();
+  for(int i=0;i<sup;i++)
+    {
+      e1.key=rand();
+      drzewko->add(e1);
+    }
+  s.stop();
+  s.pokazCzas();
 }
